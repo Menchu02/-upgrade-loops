d56a47cc59303bfb72350eb7afbf73a2d8a3d084
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2021-12-04 20:50:53 +0100

    add Iteracion#3

---
+++ script3.js
@@ -0,0 +1,20 @@
+// **Iteración #3: Probando For...of**
+
+// Usa un bucle forof para recorrer todos los destinos del array. Imprime en un ***console.log*** sus valores.
+
+// Puedes usar este array:
+const placesToTravel = [
+  'Japon',
+  'Venecia',
+  'Murcia',
+  'Santander',
+  'Filipinas',
+  'Madagascar',
+]
+
+// for (value of dieHardObj) {
+//     console.log(value);
+//   }
+for (var x of placesToTravel) {
+  console.log(x)
+}
